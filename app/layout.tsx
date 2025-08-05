f53cbb4533f40dd51fe19379678138da5cb70ee1
fernandes-draw <fernandes_draw@hotmail.com>
2025-08-05 17:02:23 -0300

    Create Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import Navbar from '@/components/Navbar';
-import { Sidebar } from 'lucide-react';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Sidebar from '@/components/Sidebar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <Navbar />
         <div className='flex'>
-          <div className='hidden md:block h-[100vh]'>
+          <div className='hidden md:block h-[100vh] w-[300px]'>
             <Sidebar />
           </div>
           <div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,72 @@
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from '@/components/ui/command';
+import {
+  Blocks,
+  FolderCog,
+  LayoutDashboard,
+  Settings,
+  Truck,
+  User,
+  UserPlus2,
+  UserStarIcon,
+} from 'lucide-react';
+import Link from 'next/link';
+
 const Sidebar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <Command className='bg-secondary rounded-none'>
+      <CommandInput placeholder='Clique num item ou busque...' />
+      <CommandList>
+        <CommandEmpty>Nenhum resultado encontrado...</CommandEmpty>
+        <CommandGroup heading='Cadastros'>
+          <CommandItem>
+            <LayoutDashboard className='mr-2 h-4 w-4' />
+            <Link href='/dashboard'>Painel de Controle</Link>
+          </CommandItem>
+          <CommandItem>
+            <Blocks className='mr-2 h-4 w-4' />
+            <Link href='/products'>Produtos</Link>
+          </CommandItem>
+          <CommandItem>
+            <FolderCog className='mr-2 h-4 w-4' />
+            <Link href='/projects'>Projetos</Link>
+          </CommandItem>
+          <CommandItem>
+            <UserStarIcon className='mr-2 h-4 w-4' />
+            <Link href='/clients'>Clientes</Link>
+          </CommandItem>
+          <CommandItem>
+            <Truck className='mr-2 h-4 w-4' />
+            <Link href='/suppliers'>Fornecedores</Link>
+          </CommandItem>
+          <CommandItem>
+            <UserPlus2 className='mr-2 h-4 w-4' />
+            <Link href='/users'>Usuários</Link>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading='Configurações'>
+          <CommandItem>
+            <User className='mr-2 h-4 w-4' />
+            <span className=''>Perfil</span>
+            <CommandShortcut>Ctrl+U</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className='mr-2 h-4 w-4' />
+            <span className=''>Configurações</span>
+            <CommandShortcut>Ctrl+C</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
   );
 };
 
